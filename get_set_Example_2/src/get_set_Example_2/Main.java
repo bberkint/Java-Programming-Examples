04711eefb1b40cdb97f90d0c84f237aaa70e5bbd
BBerkinT <71976654+bberkint@users.noreply.github.com>
2022-02-19 12:35:05 +0300

    Add files via upload

---
+++ get_set_Example_2/src/get_set_Example_2/Main.java
@@ -0,0 +1,14 @@
+package üçüncüHaftaLab1;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Account account1 = new Account(1000, "Berkin");
+		account1.withdraw(200);
+			
+	}
+
+}
+
