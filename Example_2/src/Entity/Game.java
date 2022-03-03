aaa54be350e1007ae869f39815f40385e712251a
BBerkinT <balak_berkin_32@hotmail.com>
2022-03-04 00:04:23 +0300

    Create Game.java

---
+++ Example_2/src/Entity/Game.java
@@ -0,0 +1,46 @@
+package Entity;
+
+public class Game {
+	
+	private int id;
+	private String gameName;
+	private double price;
+	
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Game(int id, String gameName, double price) {
+		super();
+		this.id = id;
+		this.gameName = gameName;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getGameName() {
+		return gameName;
+	}
+
+	public void setGameName(String gameName) {
+		this.gameName = gameName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
