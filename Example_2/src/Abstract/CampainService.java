78844308c64df6ebb74740c566e677e2b000233b
BBerkinT <balak_berkin_32@hotmail.com>
2022-03-04 00:03:15 +0300

    Create CampainService.java

---
+++ Example_2/src/Abstract/CampainService.java
@@ -0,0 +1,10 @@
+package Abstract;
+
+import Entity.Campaign;
+
+public interface CampainService {
+	void add(Campaign campaign);
+	void delete(Campaign campaign);
+	void update(Campaign campaign);
+
+}
